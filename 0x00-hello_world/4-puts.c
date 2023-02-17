05baf946d6d90817c4060210d427c8d515732405
WinnyKay3 <winffredkalondu@gmail.com>
2023-02-16 16:20:55 -0800

    Four puts .c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,2 @@
 #!/bin/bash
-/**
-* main - entry point
-* 
-* Description: prints line using puts
-*
-* Return: 0 on success
-*/
-#include <stdio.h>
-
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}		
+/** *main - entry point * * Description: prints line using puts * * Return: 0 on success */ #include <stdio.h>  int main(void) {  puts("\"Programming is like building a multilingual puzzle");  return (0); }
