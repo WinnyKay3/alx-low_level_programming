c57987868bebeeb5f7f10499be55791d3946d014
WinnyKay3 <winffredkalondu@gmail.com>
2023-02-16 16:02:17 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+/**
+* main - entry point
+* 
+* Description: prints line using puts
+*
+* Return: 0 on success
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}		
