c94007aa7d779ac73f05d6e86a8e2662b7f8a661
WinnyKay3 <winffredkalondu@gmail.com>
2023-02-16 17:34:51 -0800

    Five printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,10 @@
-#!/bin/bash
-/**
- * main - entry point function
- *
- * Description: Prints a statement using printf
- *
- * Return: Always 0 on success
- */
-
 #include <stdio.h>
-
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+/**
+ * main - Start of C program
+ * Return: 0 Success
+ */
+int main() {
+	printf("with proper grammar,but the outcome is a piece of art,\n");
+	return 0;
 }	
+
