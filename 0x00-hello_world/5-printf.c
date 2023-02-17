18c4d84ccde2ef77810498d8096c2193d7cdfe9d
WinnyKay3 <winffredkalondu@gmail.com>
2023-02-16 16:31:47 -0800

    Printf 5.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+/**
+ * main - entry point function
+ *
+ * Description: Prints a statement using printf
+ *
+ * Return: Always 0 on success
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}	
