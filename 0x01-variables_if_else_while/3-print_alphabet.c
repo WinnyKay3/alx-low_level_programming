e3dd592aae52e7098ddde0d56988c685df699bb3
WinnyKay3 <winffredkalondu@gmail.com>
2023-02-19 01:01:53 -0800

    Print alphabet.c 3

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,34 @@
+/* library stdlib and time included */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -function
+ *
+ * Description: prints the alphabet lower case then upper case
+ *
+ * Return: returns (0) on success
+ */
+
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 97; i < 123; i++)
+	{
+		c = i;
+		putchar(c);
+	}
+	for (i = 65; i < 91; i++)
+	{
+		c = i;
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
