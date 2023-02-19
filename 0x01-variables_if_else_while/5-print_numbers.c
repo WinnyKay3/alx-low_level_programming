9d477c820cd124e8d9ecc7ba32fd829b6e916574
WinnyKay3 <winffredkalondu@gmail.com>
2023-02-19 01:52:28 -0800

    Print numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+/* library stdlib and time included */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main -function
+ *
+ * Description: prints all single digit numbers of base 10
+ *
+ * Return: returns (0) on success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
