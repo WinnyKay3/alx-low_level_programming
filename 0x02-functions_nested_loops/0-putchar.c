c1c798cbd6a5c8c44f1f223cdd7ae43192c45623
WinnyKay3 <winffredkalondu@gmail.com>
2023-02-21 05:57:21 -0800

    Putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Description: prints putchar using putchar function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
