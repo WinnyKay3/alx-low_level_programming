8e9a9151f187340668e4f621302f82c4d8082c25
WinnyKay3 <winffredkalondu@gmail.com>
2023-02-21 08:30:11 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - alphabets in lower case
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+char j;
+for (j = 'b'; j <= 'p'; j++)
+_putchar(j);
+_putchar('\n');
+}
