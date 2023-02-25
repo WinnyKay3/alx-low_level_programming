f0341a3f7462c6db779fdbda4063482daa9c2663
WinnyKay3 <winffredkalondu@gmail.com>
2023-02-24 18:55:08 -0800

    Five more numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - void function
+ *
+ * Definition: Prints numbers 0 to 14 10 times
+ *
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			{
+				_putchar('0' + (j / 10));
+			}
+			_putchar('0' + (j % 10));
+		}
+		_putchar('\n');
+	}
+}
