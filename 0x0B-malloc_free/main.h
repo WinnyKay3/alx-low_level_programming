f68a123dca114a13f7abb11f401e83a094f24a66
WinnyKay3 <winffredkalondu@gmail.com>
2023-03-14 03:42:41 -0700

    Main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,9 +3,8 @@
 /**
  *
  * Desc: header file containing all prototypes and functions used in 0x0B- malloc free directory.
- *
  */
-int _putchar(char c)
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
