a8a36ab4110d135a9a6c44caf8892d9fd3845408
WinnyKay3 <winffredkalondu@gmail.com>
2023-06-09 10:42:08 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,7 +5,7 @@
  * @size: size of the array
  *
  * Return: if an error - NULL
- * 		Otherwise pointer to newly created hash table
+ * Otherwise pointer to newly created hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
